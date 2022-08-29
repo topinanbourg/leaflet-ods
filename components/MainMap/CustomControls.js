6bf96e6768d7574108bfb6aa030fdcbc5dce88bd
JosNo <josselin.nauche@gmail.com>
2022-08-30 01:11:37 +0200

    cleanup useless code

--- components/MainMap/CustomControls.js
+++ components/MainMap/CustomControls.js
@@ -1,43 +1,19 @@
 
-import { Marker } from "leaflet";
-import { useMap, useMapEvents, ZoomControl } from "react-leaflet";
+import { useMapEvents, ZoomControl } from "react-leaflet";
 
 const CustomControls = ({ onClick }) => {
-    const map = useMap();
-
-    const handleZoomIn = () => {
-        map.zoomIn();
-    };
-
-    const handleZoomOut = () => {
-        map.zoomOut();
-    };
-
     // Call useMapEvents:
     const mapEvents = useMapEvents({
         // Use leaflet map event as the key and a call back with the 
         // map method as the value:
-        zoomend: () => {
-            // Get the zoom level once zoom ended:
-            console.log(map.getZoom())
-        },
         click: (e) => {
             // Get bounds once move has ended:
-            //console.log(e)
             onClick(e.latlng);
         }
     })
 
     return (
         <>
-            <div className="custom-controls">
-                <button className="custom-controls__button" onClick={handleZoomIn}>
-                    +
-                </button>
-                <button className="custom-controls__button" onClick={handleZoomOut}>
-                    -
-                </button>
-            </div>
             <ZoomControl position="bottomright" />
         </>
     );
